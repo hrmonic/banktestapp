c2aec5473cf38b6e1e44842115f9cda27d46c1d4
hrmonic <hrmonicmusic@gmail.com>
2025-08-10 00:56:34 -0700

    ffix(starter): wrap App with AuthProvider and PermissionsProvider

--- apps/starter/src/main.jsx
+++ apps/starter/src/main.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { AuthProvider } from "@/lib/auth/authProvider.js";
+import { PermissionsProvider } from "@/lib/security/rbac.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    <Browser>Router>
+      <AuthProvider>
+        <PermissionsProvider>
+          <App />
+        </PermissionsProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
