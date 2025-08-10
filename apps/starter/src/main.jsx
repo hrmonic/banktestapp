9ed42c790d3ad7e048f5f2c03b4aac0182af7f57
hrmonic <hrmonicmusic@gmail.com>
2025-08-10 01:00:19 -0700

    fix(starter): correct imports in main.jsx

--- apps/starter/src/main.jsx
+++ apps/starter/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "@/lib/auth/authProvider.js";
-import { PermissionsProvider } from "@/lib/security/rbac.js";
+import { PermissionsProvder } from "@/lib/security/rbac.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
